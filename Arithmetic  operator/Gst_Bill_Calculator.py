05e6213c4264d282c018e594a04376beb1d2d36f
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-08-14 12:38:32 +0530

    Data Type- int,str,boll,float

---
+++ Arithmetic  operator/Gst_Bill_Calculator.py
@@ -0,0 +1,13 @@
+item1 = 678
+item2 = 234
+item3 = 450
+
+
+subtotal = item1 +item2 + item3
+gst = subtotal * 18 /100
+final_bill = subtotal + gst
+
+
+print("Subtotal:" , subtotal)
+print("GST AMOUNT", gst)
+print("Final Bill:", final_bill)
---
+++ variable & data type/data1.py
@@ -0,0 +1,11 @@
+#Check data type
+name = "vaibhav rathore"
+age = 21
+pi = 3.14
+student_1 = True
+
+print(type(name))
+print(type(age))
+print(type(pi))
+print(type(student_1))
+
