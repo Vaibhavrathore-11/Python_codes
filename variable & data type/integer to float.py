933720e2ce598a0ca97b117095c952a2c53f96ef
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-08-14 12:47:44 +0530

    int-> float convrt , float-> int

---
+++ variable & data type/integer to float.py
@@ -0,0 +1,14 @@
+#integer to float convertor
+num = 100
+result = float(num)
+print(result)
+print(type(result))
+
+#float to int
+
+marks = 76.4
+value = int(marks)
+
+print(value)
+print(type(value))
+
