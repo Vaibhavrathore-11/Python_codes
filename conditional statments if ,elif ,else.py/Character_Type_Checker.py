98ee64f7c0b23a4b5d5f5f0513f7799fe807172f
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-08-27 17:10:04 +0530

    Add character type checker using conditional statements

---
+++ conditional statments if ,elif ,else.py/Character_Type_Checker.py
@@ -0,0 +1,17 @@
+character = input("Enter a Character")
+
+if character.isupper():
+    print("Uppercase")
+
+elif character.islower():
+      print("Lower Case")    
+elif character.isdigit():
+     print("Digit")
+
+else:
+     print("Special Character")     
+
+
+ #Lower case - rathore
+ # UPPERCASE - RATHORE
+ # special charecter - @#$%^    
