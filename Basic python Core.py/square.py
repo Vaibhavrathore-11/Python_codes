c9281fc9c3e0bbc5b537b0c92f127a9c9db7e2b9
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-16 15:43:33 +0530

    square root number

---
+++ Basic python Core.py/square.py
@@ -0,0 +1,37 @@
+#write a python program to calculate the seuare of number enterd by the user
+
+##print("The square of these two number", a*a)
+
+
+#Example - square of input  number
+
+b = int(input("enter your number"))
+square = (b * b )
+print("The square of these two number ",square )
+
+# find a square of 23,44,56,12,15 using user input
+
+a = int(input("Enter a your number:"))
+b = int(input("Enter a your number:"))
+c = int(input("Enter a your number:"))
+d = int(input("Enter a your number:"))
+e = int(input("Enter a your number:"))
+
+print("The square of number is" , a*a)
+print("The square of number is" , b*b)
+print("The square of number is" , c*c)
+print("The square of number is" , d*d)
+print("The square of number is" , e*e)
+
+
+
+#write a python program to calculate the seuare of number enterd by the user
+
+##print("The square of these two number", a*a)
+
+
+#Example - square of input  number
+
+b = int(input("enter your number"))
+square = (b * b )
+print("The square of these two number ",square )
