c497214a8bd7fbbc6e50e428db00213c83381b1c
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-16 17:21:32 +0530

    length of string and slicing of string

---
+++ string/11_problem.py
@@ -0,0 +1,25 @@
+# find a length of string
+
+str = "Aurobindo"
+print(str)
+print(len(str)) # output - 9
+
+str1 = "Harry porter"
+print(len(str1))
+
+#Slicing of string
+
+name = "Institute"
+str = name[0:5] # start from index 0 all the way till 3 (excluding 3)
+print(str)
+str1 = name[0:6]
+print(str1)
+str2 = name[1:6]
+print(str2)
+
+student  = "jogindar"
+str = student[0:4] 
+str1 = student[3:8]
+print(str)
+print(str1)
+
