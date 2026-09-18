3b123bd3f39fa6e8decf303021fc6eb073d38035
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-18 20:54:47 +0530

    use Capitalize function()

---
+++ Basic python Core.py/capitalized.py
@@ -0,0 +1,20 @@
+# string capitalize() - This functions capitalize the first Character of given string 
+
+name = "aurobindo institute of technology"
+str = name.capitalize()
+print(str)
+
+
+value = "friends"
+str1 = (value.capitalize())
+print(str1)
+
+
+# User input capitalize()
+val = input("Enter your name:")
+str2 = (val.capitalize())
+print(str2)
+
+#Capitalize first letter ko capital baaki ko small letter me convert karta ha 
+nam = "vAIBHAV"
+print(nam.capitalize()) #Vaibhav
