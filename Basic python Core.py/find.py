cc076c7861d6268dab9c25804db3d6f3a640395a
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-18 21:11:36 +0530

    use find () function

---
+++ Basic python Core.py/find.py
@@ -0,0 +1,29 @@
+s = "Hello World"
+index = s.find("World")
+print(index) # 6
+
+
+# function  - string.find(word)
+
+name = "Vaibhav Rathore"
+str = name.find("Rathore")
+print(str)
+
+#code
+sentence = "I love python programming"
+var = sentence.find("python")
+print(var)
+
+#code
+sentance1 = "programming"
+var1 = sentance1.find("g")
+print(var1)
+
+ # code
+sentence = "python is easy and python is powerful"
+value = sentence.find("python")
+print(value)
+
+ #code
+sentence = "I love python and I learn python"
+print(sentence.find("python",8))
