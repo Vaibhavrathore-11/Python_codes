89d7d3f69a4a72a459faab6c1044897a0ac2baeb
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-18 20:21:13 +0530

    using endswith function

---
+++ Basic python Core.py/length.py
@@ -0,0 +1,51 @@
+# find a length of string
+
+str = "Aurobindo"
+print(str)
+print(len(str)) # output - 9
+
+str1 = "Harry porter"
+print(len(str1))
+
+name = "RathoreVaibhav"
+print(len(name))
+
+
+#Find a list length 
+fruits = ["Apple","Mango","Banana","Gavava", "berry",'chiku']
+print(fruits)
+print(len(fruits))
+
+x = ""
+print(len(x[1:5]))
+
+# Take a user input in 5 numbers list and print(length)
+
+nums = []
+
+for i in range(5):
+    num = int(input("Enter number:"))
+    nums.append(nums)
+
+    print(nums)
+    print("Length:", len(nums))
+
+
+nums1 = []
+for i in range(10):
+        num = input("Enter number:")
+        nums1.append(num)
+        print(nums1)
+
+
+
+namee = "harsaha"
+harry ="ganesh"
+print(len(name))
+print(namee.endswith("saha"))
+print(harry.endswith("esh"))
+
+#endswith
+name = "jack"
+print(len(name))
+print(name.endswith("ck"))
