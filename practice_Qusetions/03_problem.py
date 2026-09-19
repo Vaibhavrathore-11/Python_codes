5f754ff3b581357d3db794588b90980df13fa0c9
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-19 14:36:02 +0530

    using find () function to find double space and string character

---
+++ practice_Qusetions/03_problem.py
@@ -0,0 +1,7 @@
+# write a program to detect double space in a string
+
+program  = "i am good person  and  enginnering student"
+
+print(program.find("  "))
+print(program.find("good"))
+print(program.find("enginnering"))
