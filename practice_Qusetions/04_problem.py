180186f59f9407a5b35aa9124235e108111ecedb
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-19 14:45:32 +0530

    using repalce function for removing extra space

---
+++ practice_Qusetions/04_problem.py
@@ -0,0 +1,9 @@
+# Question -1
+# Replace the double space from problem 3 with single space replace()
+
+name = "Rajat  Patidar is  a cricket  player"
+
+var =(name.replace("  ", " "))
+print(var)
+
+#output - Rajat Patidar is a cricket player (single space)
