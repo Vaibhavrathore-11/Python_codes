f08d6ebee2786bc55f650f6eaf83408e6d8bcf99
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-19 13:25:03 +0530

    escape_sequence_character

---
+++ Basic python Core.py/escape_seq.py
@@ -0,0 +1,17 @@
+a = "Dhanak is a good boy\n but not a bad boy" 
+print(a) # use backslash \n escape character 
+
+
+b = "ganesh is a smart\t but he is not a mindless"
+print(b) #escape sequence character \t for tabuler space
+
+#double code \""\ will be manuplate double code
+
+c = "He is a boy but he is not a \"man\"" #use double code
+
+d = 'This is wonderful \'Place\'' # single quote
+print(d)
+print(c)
+
+e = "hello\bsir i am vaibhav"
+print(e)
