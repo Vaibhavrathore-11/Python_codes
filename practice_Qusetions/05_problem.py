fefdeae60cdea1c5145c00ca33ae0bcb5c9683fa
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-19 14:58:31 +0530

    use escape sequence characters functions()

---
+++ practice_Qusetions/05_problem.py
@@ -0,0 +1,10 @@
+# Question - Write a program to formate the following letter using escape sequence characters
+
+#Letter = "Dear , Vaibhav, this python is very easy. Thanks!"
+
+letter = "Dear Vaibhav\n \t This python is very easy.\nThanks!"
+print(letter)
+
+
+letter2 = "Hello friend \n \t i am your bestfriend \n please talk with me please. \n\tThankyou "
+print(letter2)
