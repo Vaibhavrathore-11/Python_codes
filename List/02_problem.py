e825dcfd4b63cee7c8d960f60d00bc716974a435
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-20 09:46:30 +0530

    Slicing in list

---
+++ List/02_problem.py
@@ -0,0 +1,16 @@
+#Slicing of List
+number = [12,34,66,88,98,44,13]
+number[0] = 14
+print(number)
+print(number[4])
+#Slicing of list 
+print(number[0])
+print(number[3:6])
+print(number[0:])
+print(number[0:5])
+print(number[3:7])
+
+num = [1,2,4,6,78,65,33,22,]
+print(num[0::2])
+print(num[1::3])
+
