fda93d534607543dd8d335e857ba144afc5e3bce
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-20 11:51:38 +0530

    Add string reverse using reverse() method

---
+++ List/05_problem.py
@@ -0,0 +1,11 @@
+#Use Reverse function () for reverse list element
+
+l1 = [122,132,45,67,38, 26,77]
+l1.reverse()
+print(l1)
+
+value = list(input("Enter your name:"))
+
+value.reverse()
+print(value)
+print("".join(value))
