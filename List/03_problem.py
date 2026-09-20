d6e0b2b2e2945c0deaa2d20d75200b2aad50b149
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-20 10:07:28 +0530

    useing-> append function

---
+++ List/03_problem.py
@@ -0,0 +1,11 @@
+#append value in string
+friends = ["vaibhav","Sourabh","Om","Harshu" ]
+
+friends.append("Lucky") #append value in list
+print(friends)
+
+
+#Questionq
+fruit = ["Apple","gavava","Mango","Chiku","banana"]
+fruit.append("Watermelone")
+print(fruit)
