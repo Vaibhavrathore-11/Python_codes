784d4d9dab9c5fa647ac56da0489adee9a1cb4ee
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-20 09:42:05 +0530

    find indexing in string

---
+++ List/01_problem.py
@@ -0,0 +1,8 @@
+# list (indexing)
+
+friends = ["Apple","orange","3.14","Aakash", 23, False]
+print(friends)
+print(friends[3]) # indexing
+print(friends[2])
+friends[1] = "Grapes" #assign additional value in string
+print(friends)
