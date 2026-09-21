94ad7319143a274485678880c0fcbcffb5ec582a
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-21 09:37:23 +0530

    Use insert() function for insert value in list

---
+++ List/06_problem.py
@@ -0,0 +1,17 @@
+#use insert function in list
+
+valu = [12,14,16,19,22,26]
+
+valu.insert(3 , 18)
+print(valu)
+
+
+#Question- 2
+
+a = [12,45,67,88,44,33,22,11]
+b = [24,56,47,78,84,66,44,23]
+a.insert(3,19) # insert 19 such that its index in the list insert value without change a any value in list
+b.insert(3,19)
+
+print(a)
+print(b)
