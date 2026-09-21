91aa101c9ef24b57a77564709cc8387ff34b1bc2
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-21 09:52:08 +0530

    Add Python pop() function practice

---
+++ List/08_problem.py
@@ -0,0 +1,8 @@
+#Write a Python program that takes a list of numbers from the user, removes the last element using the pop() function, and prints both the removed element and the updated list.
+
+
+l2 = [10,20,30,40,50]
+print(l2)
+value = l2.pop()
+print(value)
+print(l2)
