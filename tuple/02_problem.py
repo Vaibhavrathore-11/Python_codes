be0885d2effa15f2722324f5fa4bb622ef3ddaa7
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-22 14:02:53 +0530

    Practice count() function with multiple values

---
+++ tuple/02_problem.py
@@ -0,0 +1,24 @@
+a = (1,45,2221,4554,45,"box")
+
+print(a)
+value = a.count(45)
+print(value) # output - 2
+
+sentence = ("python is easy . I love python and python programming")
+count = sentence.count("python") 
+print(count)
+
+#Python is amazing and Python is easy to learn.
+'''Write a Python program that takes a sentence from the user and counts:
+
+How many times the word python appears.
+How many times the letter a appears.
+Print both counts separately.'''
+
+sentencee = ("python is amazing and python is easy to learn")
+
+print(sentencee)
+print(sentencee.count("python"))
+print(sentencee.count("a"))
+
+
