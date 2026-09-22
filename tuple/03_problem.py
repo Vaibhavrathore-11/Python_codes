e9edc9f51f47f00600bc14db0192ceedd745f981
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-22 14:26:13 +0530

    Practice tuple index() method

---
+++ tuple/03_problem.py
@@ -0,0 +1,7 @@
+
+# Create a tuple containing 7 different fruits. Take a fruit name from the user and use the index() method to find its position in the tuple.
+
+Fruits = ("Apple", "kiwi", "Banana", "berry","lichi","Gavava","Mango")
+print(Fruits)
+print(Fruits.index("Banana"))
+print(Fruits.index("Mango"))
