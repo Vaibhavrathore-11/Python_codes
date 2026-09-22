4763bb13f62574fe35b058ebbdb2d138cf74a146
vaibhavrathore87563-spec <vaibhavrathore87563@gmail.com>
2026-09-22 22:12:14 +0530

    Practice tuple repetition using * operator 🚀

---
+++ tuple/06_problem.py
@@ -0,0 +1,22 @@
+#Repetition Tuples can be repeated using the '*' operator 
+
+my_tuple = (1, 2, 3)
+repeated = my_tuple * 2
+
+print(repeated)
+
+
+#Question 
+'''Create a tuple containing 5 numbers. Use the tuple repetition operator to repeat the entire tuple 3 times.
+
+Print the original tuple.
+Print the repeated tuple.
+Use len() to find the total number of elements after repetition.'''
+
+tup = (2,3,4,6,7)
+print(tup)
+
+repeat = tup * 3
+print(repeat)
+
+print(len(repeat))
